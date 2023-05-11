7a1332179572b80897d0c065e5fe7b8b5f90575f
Ndonwi Rhema <rhemandonwi59@gmail.com>
2023-05-11 16:01:50 +0530

    inifnity Grocery Website

---
+++ src/Components/Banner/BannerHead.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './BannerHead.css'
+
+const BannerHead = () => {
+  return (
+    <div className='banner_container'>
+        BannerHead
+    </div>
+  )
+}
+
+export default BannerHead
