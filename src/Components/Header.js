29db0145841a491eb5a710e6bada578642863ac4
Ndonwi Rhema <rhemandonwi59@gmail.com>
2023-05-11 16:02:02 +0530

    First commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BannerHead from './Banner/BannerHead'
+
+const Header = () => {
+  return (
+    <div>
+      <BannerHead/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/screens/Home/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../../Components/Header'
+
+
+const HomeScreen = () => {
+  return (
+   <div className='container'>
+        {/* // Header Section .. */}
+            <Header/>
+        {/*  Carousel Section .. */}
+
+        {/*  Promotions Section.. */}
+
+        {/*  Product Categories Section .. */}
+
+        {/*  Featured Products.. */}
+
+        {/*  Footer Section .. */}
+        
+    </div>
+  )
+}
+
+export default HomeScreen
